c3945f8adad900e42caa8205d2ce01c3074fcd96
ittrainbow <deadbead@gmail.com>
2020-12-06 16:30:18 +0300

    Глава 08 01 начало

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ApplicationRecord
+    validates :title, :description, :image_url, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+    validates :title, uniqueness: true
+    validates :image_url, allow_blank: true, format: {
+        with: %r{\.(gif|jpg|png)\z}i,
+        message: 'must be a URL for GIF, JPG or PNG image.'
+        }
+
+    # Глава 07 задание
+    validates :title, length: { minimum: 2 }
+end
