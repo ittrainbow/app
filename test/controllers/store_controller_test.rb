c70b2f15653c2b7f0d7d411a72278991619e680f
ittrainbow <deadbead@gmail.com>
2020-12-08 03:01:57 +0300

    Глава 09 01 начало

---
+++ test/controllers/store_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class StoreControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get store_index_url
+    assert_response :success
+    assert_select 'nav.side_nav a', minimum: 2
+    assert_select 'main ul.catalog li', 3
+    assert_select 'h2', 'Programming Ruby 1.9'
+    assert_select '.price', '10.00 ₽'
+  end
+
+end
