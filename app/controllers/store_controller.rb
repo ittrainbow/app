d6a8e531b1a7280782e406276982914f5e164f44
ittrainbow <deadbead@gmail.com>
2020-12-12 02:47:24 +0300

    Глава 10 01 начало

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,24 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+
+  def counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+  end
+
+  def counter_message
+    session[:message] = 'Visit #'
+  end
+
   def index
     @products = Product.order(:title)
     @time = Date.today
+
+    @count = counter
+    @show_msg1 = counter_message if @count > 5
+    @show_msg2 = @count if @count > 5
   end
 end
