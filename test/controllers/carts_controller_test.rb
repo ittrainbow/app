b47e3a8be75f3ba15754660397f39098603c7157
ittrainbow <deadbead@gmail.com>
2020-12-21 07:25:30 +0300

    Глава 12 03 итерация G2

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -11,6 +11,8 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    post line_items_url, params: { product_id: products(:ruby).id }
+
     get new_cart_url
     assert_response :success
   end
@@ -49,6 +51,12 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to carts_url
+  end
+
+  test "requires item in cart" do
+    get new_order_url
+    assert_redirected_to store_index_path
+    assert_equal flash[:notice], 'Your cart is empty'
   end
 
   # test "add duplicate products" do
