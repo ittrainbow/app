22d6fd0e70317aa9b24eebbbac9233c4edc2cddc
ittrainbow <deadbead@gmail.com>
2021-01-09 03:24:35 +0300

    Глава 15 02 итерация J2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+
+Product.create(
+    title: 'Rails 1',
+    description: %{Книга 1. Первый текст для объема, не несущий никакой полезной информации},
+    image_url: 'ruby1.png',
+    price: 150
+)
+
+Product.create(
+    title: 'Rails 2',
+    description: %{Книга 2. Второй текст для объема, не несущий никакой полезной информации},
+    image_url: 'ruby2.png',
+    price: 200
+)
+
+Product.create(
+    title: 'Rails 3',
+    description: %{Книга 3. Третий текст для объема, не несущий никакой полезной информации},
+    image_url: 'ruby3.png',
+    price: 250
+)
